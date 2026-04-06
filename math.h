5721ed2504f539ade2f6db3e6c334469fc6c3df7
Gehrman <underpivo@gmail.com>
2026-04-06 14:54:58 +0300

    Add Math class for basic mathematical operations

---
+++ math.h
@@ -0,0 +1,13 @@
+/**
+ * \brief Класс для математических операций.
+ */
+class Math {
+public:
+    /**
+     * \brief Суммирует два числа.
+     * \param a Первое число.
+     * \param b Второе число.
+     * \return Сумма a и b.
+     */
+    int sum(int a, int b) { return a + b; }
+};

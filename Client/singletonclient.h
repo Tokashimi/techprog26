bf1493a59c3ba3aaf9324eda8595a173c02df11b
mm <ss>
2026-04-26 15:13:36 +0300

    тудудуду

---
+++ Client/singletonclient.h
@@ -0,0 +1,48 @@
+#ifndef SINGLETONCLIENT_H
+#define SINGLETONCLIENT_H
+
+#include <QObject>
+#include <QTcpSocket>
+
+class SingletonClient;
+
+// Destroyer - автоматически удаляет синглтон при завершении программы
+class SingletonDestroyer
+{
+private:
+    SingletonClient* p_instance;
+public:
+    ~SingletonDestroyer() { delete p_instance; }
+    void initialize(SingletonClient* p) { p_instance = p; }
+};
+
+class SingletonClient : public QObject
+{
+    Q_OBJECT
+private:
+    static SingletonClient*   p_instance; // единственный экземпляр
+    static SingletonDestroyer destroyer;  // удалит экземпляр при выходе
+    QTcpSocket* mTcpSocket;               // сокет для связи с сервером
+
+protected:
+    explicit SingletonClient(QObject* parent = nullptr); // запрос на подключение - в конструкторе
+    SingletonClient(SingletonClient&) = delete;
+    SingletonClient& operator=(SingletonClient&) = delete;
+    ~SingletonClient(); // отключение - в деструкторе
+    friend class SingletonDestroyer;
+
+public:
+    static SingletonClient* getInstance(); // единственная точка доступа к объекту
+
+    Q_INVOKABLE void send_msg_to_server(QString query); // отправка сообщений на сервер - функция
+    Q_INVOKABLE void send_calc_h(double a, double b, int n); // шаг интегрирования
+
+signals:
+    void message_from_server(QString msg); // сигнал при получении ответа от сервера
+
+private slots:
+    void slotServerRead();   // обработка сообщений от сервера - слот
+    void slotDisconnected(); // отключение при отключении сервера
+};
+
+#endif // SINGLETONCLIENT_H
